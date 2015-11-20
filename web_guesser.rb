44ad7b6c2d04b3dad9ca51c7b61bcd000e0d2763
James Crockett <jecrockett@gmail.com>
2015-11-20 13:23:21 -0700

    colorized the output

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,17 +6,32 @@ SECRET_NUMBER = rand(100)
 get '/' do
   guess = params['guess']
   message = check_guess(guess)
-  erb :index, :locals => { :message => message }
+  background_color = check_background_color(message)
+  erb :index, :locals => { :message => message, :background_color => background_color }
 end
 
-def check_guess(guess)
-  if guess.nil?
-    message = ""
-  elsif guess.to_i > SECRET_NUMBER
-    (guess.to_i - SECRET_NUMBER) > 5 ? message = "Way too high!" : message = "Too high!"
-  elsif guess.to_i < SECRET_NUMBER
-    (SECRET_NUMBER - guess.to_i) > 5 ? message = "Way too low!" : message = "Too low!"
-  else
-    message = "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}."
+  def check_guess(guess)
+    if guess.nil?
+      message = ""
+    elsif guess.to_i > SECRET_NUMBER
+      (guess.to_i - SECRET_NUMBER) > 5 ? message = "Way too high!" : message = "Too high!"
+    elsif guess.to_i < SECRET_NUMBER
+      (SECRET_NUMBER - guess.to_i) > 5 ? message = "Way too low!" : message = "Too low!"
+    else
+      message = "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}."
+    end
   end
+
+  def check_background_color(message)
+    if message.empty?
+      "white"
+    elsif message.split.include?("Way")
+      "#FF3036"
+    elsif message.split.include?("right!")
+      "#56ce56"
+    else
+      "#ff9090"
+
+  end
+
 end
