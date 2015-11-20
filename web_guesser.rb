653fe9e273aca95442843364ab166bf53a462436
James Crockett <jecrockett@gmail.com>
2015-11-20 13:04:53 -0700

    refactored check guess method

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_number = rand(101)
+SECRET_NUMBER = rand(100)
 
 get '/' do
-  "The SECRET NUMBER is #{random_number}."
+  guess = params['guess']
+  message = check_guess(guess)
+  erb :index, :locals => { :message => message }
+end
+
+def check_guess(guess)
+  if guess.nil?
+    message = ""
+  elsif guess.to_i > SECRET_NUMBER
+    (guess.to_i - SECRET_NUMBER) > 5 ? message = "Way too high!" : message = "Too high!"
+  elsif guess.to_i < SECRET_NUMBER
+    (SECRET_NUMBER - guess.to_i) > 5 ? message = "Way too low!" : message = "Too low!"
+  else
+    message = "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}."
+  end
 end
